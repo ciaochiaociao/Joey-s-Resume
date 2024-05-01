cb9cac36106845c14dff1818d7e367837d400bd6
ciaochiaociao <13917478+ciaochiaociao@users.noreply.github.com>
2024-05-01 13:55:13 -0500

    add cv related experience and project

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 # latexmk -synctex=1 -interaction=nonstopmode -file-line-error -pdf
-latexmk -synctex=1 -interaction=batchmode -pdf && latexmk -c && copy main.pdf Chiao-Wei_Hsu_resume.pdf
+latexmk -synctex=1 -interaction=batchmode -pdf && latexmk -c && copy main.pdf Chiao-Wei_Hsu_cv_resume.pdf
